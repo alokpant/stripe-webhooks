574bd13e965a9fb0f66835958d048de6809edd3b
Alok <justtodesign@gmail.com>
2023-11-27 03:46:01 +0100

    Update user to also create invoice

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,21 @@ class User < ApplicationRecord
   end
 
   after_create do
-    Stripe.api_key = Rails.application.credentials.stripe[:development][:secret_key]
     customer = Stripe::Customer.create(email: email)
     plan = Stripe::Plan.list
-    subscription = Stripe::Subscription.create(
-      customer: customer.id,
-      items: [{
-        price: plan.first.id,
-      }],
-      payment_behavior: 'default_incomplete',
-      expand: ['latest_invoice.payment_intent']
-    )
-    subscription.save
+    ActiveRecord::Base.transaction do
+      subscription = Stripe::Subscription.create(
+        customer: customer.id,
+        items: [{
+          price: plan.first.id,
+        }],
+        payment_behavior: 'default_incomplete',
+        expand: ['latest_invoice.payment_intent']
+      )
+      Stripe::Invoice.create({
+        customer: subscription.customer.id,
+        subscription: subscription.id
+      })
+    end
   end
 end
