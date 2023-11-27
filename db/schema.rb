c189a6b3ea72fd86942e161473b07de4aa9bdfd0
Alok <justtodesign@gmail.com>
2023-11-27 01:59:28 +0100

    Update schema as postgres is added

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_26_223233) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "subscriptions", force: :cascade do |t|
     t.string "stripe_id"
     t.string "status", default: "unpaid"
