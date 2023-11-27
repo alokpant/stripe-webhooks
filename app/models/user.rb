42c8babf210f82659681ba4dfdcd793f1bc07023
Alok <justtodesign@gmail.com>
2023-11-27 21:30:34 +0100

    Update user to remove creation of manual subscription and invoices

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,5 @@ class User < ApplicationRecord
 
   after_create do
     customer = Stripe::Customer.create(email: email)
-    plan = Stripe::Plan.list
-    ActiveRecord::Base.transaction do
-      subscription = Stripe::Subscription.create(
-        customer: customer.id,
-        items: [{
-          price: plan.first.id,
-        }],
-        payment_behavior: 'default_incomplete',
-        expand: ['latest_invoice.payment_intent']
-      )
-      Stripe::Invoice.create({
-        customer: subscription.customer.id,
-        subscription: subscription.id
-      })
-    end
   end
 end
