72a4abb5b487a5c70ed13593c071fabe1edb1edf
Alok <justtodesign@gmail.com>
2023-11-27 01:58:54 +0100

    Add a callback to user to create subscription when new user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def to_s
+    email
+  end
+
+  after_create do
+    Stripe.api_key = Rails.application.credentials.stripe[:development][:secret_key]
+    customer = Stripe::Customer.create(email: email)
+    plan = Stripe::Plan.list
+    subscription = Stripe::Subscription.create(
+      customer: customer.id,
+      items: [{
+        price: plan.first.id,
+      }],
+      payment_behavior: 'default_incomplete',
+      expand: ['latest_invoice.payment_intent']
+    )
+    subscription.save
+  end
 end
