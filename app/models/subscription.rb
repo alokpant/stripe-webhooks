0c5dbe02d16e30e3619fd3eded8c0999a3cc0ff8
Alok <justtodesign@gmail.com>
2023-11-28 00:18:29 +0100

    Update layout

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,11 @@ class Subscription < ApplicationRecord
   enum :status, { unpaid: 'unpaid', paid: 'paid', canceled: 'canceled'}, default: :unpaid
 
   STRIPE_LOOKUP_KEYS = %w[monthly_one_off yearly_one_off monthly_recurring yearly_recurring].freeze
+  SUBSCRIPTION_BUTTON_COLORS = {
+    unpaid: 'orange',
+    paid: 'green',
+    canceled: 'yellow'
+  }.freeze
 
   validates_presence_of :stripe_id, :status
 end
